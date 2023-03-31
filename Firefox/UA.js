85cec8154bc99bcdda16c00225f97c93e71f70f0
DM <happydm09@naver.com>
2023-04-01 08:57:45 +0900

    Update UA.js

--- Firefox/UA.js
+++ Firefox/UA.js
@@ -6,7 +6,7 @@
 					var val = details.requestHeaders[i]
 					if (val.value.includes("Mobile")) {
 						// Mobile
-						val.value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 EdgA/96.0.1054.34';
+						val.value = 'Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.116 Mobile Safari/537.36 EdgA/110.0.1587.66';
 					} else {
 						// Other
 						val.value = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54';
