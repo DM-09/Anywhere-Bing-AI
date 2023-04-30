c8b81560e16897cdf558082be9c5fb1412a50dfb
DM <happydm09@naver.com>
2023-04-30 10:59:34 +0900

    Update main.js

--- Chrome/main.js
+++ Chrome/main.js
@@ -4,16 +4,37 @@ document.getElementById('Go').addEventListener('click', function() {
     })
 });
 
-document.getElementById('Dark').addEventListener('click', function() {
-   chrome.tabs.executeScript({
+document.getElementById('mode').addEventListener('click', function() {
+	var el = document.getElementById('mode')
+	var get = localStorage.getItem('Mode');
+	if (get == 1) {
+	  el.setAttribute('class', 'bi bi-sun')
+	  chrome.tabs.executeScript({
+        code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/light.js';document.body.appendChild(script);`
+      });
+	  localStorage.setItem('Mode', '0');
+	} else {
+	 el.setAttribute('class', 'bi bi-moon-stars')
+     chrome.tabs.executeScript({
         code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/dark.js';document.body.appendChild(script);`
-    });
-	localStorage.setItem('Mode', '1');
+     });
+	 localStorage.setItem('Mode', '1');
+	}
 });
 
-document.getElementById('Light').addEventListener('click', function() {
-   chrome.tabs.executeScript({
-        code: `var script = document.createElement('script'); script.src = 'https://happydm09.github.io/Page/test/light.js';document.body.appendChild(script);`
-    });
-	localStorage.setItem('Mode', '0');
+document.getElementById('len').addEventListener('click', function() {
+	var get = localStorage.getItem('Len');
+	var inp = prompt('Enter the max lenght. 최대 길이를 입력하세요.', get);
+	localStorage.setItem('Len', inp);
 });
+
+window.onload = function(){
+    var el = document.getElementById('mode')
+	var get = localStorage.getItem('Mode');
+	
+	if (get == 1) {
+		el.setAttribute('class', 'bi bi-moon-stars')
+	} else {
+		el.setAttribute('class', 'bi bi-sun')
+	}
+}
