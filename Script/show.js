75cfd7ab35b1d89cf2fbace56b752dd69beb69a2
DM <happydm09@naver.com>
2023-05-30 20:46:09 +0900

    Create show.js

---
+++ Script/show.js
@@ -0,0 +1,10 @@
+Show = setInterval(function() {
+  var n = 0
+  try{
+    var firstElement = document.querySelector(".cib-serp-main").shadowRoot;
+    var panel = firstElement.querySelector("cib-side-panel");
+    panel.setAttribute('style', 'display: block')
+    n = 1
+	}catch(e){}
+  if (n != 0) { clearInterval(Show) }
+}, 200);
